81917d3aaea0254e926ba9d924b6439d38b57305
untca <pankovalxndr@gmail.com>
2019-08-27 20:28:46 +0300

    Hello World для Packagist (домашния задания OTUS)

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use APankov\Hello\HelloWorld;
+echo HelloWorld::hello();
---
+++ src/APankov/HelloWorld.php
@@ -0,0 +1,13 @@
+<?
+
+namespace APankov\Hello;
+
+class HelloWorld
+{
+    public static function hello()
+    {
+        return 'Hello World!';
+    }
+}
+
+
